982bdf882754a027103a6942df64cbb2394d1998
Hrvoje Hodak <hhodak@foi.hr>
2019-10-30 16:26:34 +0100

    Napravljena kretnja objekta u desno
    Objekt se kreće u desno konstantnom brzinom. Trenutno ne sadrži sprite animaciju.

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+    }
+}
