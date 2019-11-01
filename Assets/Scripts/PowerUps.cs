a750f010035040ebc627fc48a5c75225933625c7
ghubak <ghubak@foi.hr>
2019-11-01 21:52:11 +0100

    Skupljanje objekata
    Dodan je dodatni objekt za skupljanje. Dodana je skripta koja upravlja ponašanjem kod interakcije igrača s objektom koji se može skupiti. Objekt se nakon sudara uništava.

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp();
+        }
+
+    }
+
+    private void PickUp()
+    {
+        Debug.Log("Pokupljeno");
+        Destroy(gameObject);
+    }
+}
