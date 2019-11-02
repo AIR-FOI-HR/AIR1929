dd076ce7113e8f13d33bfde59e5c83b2ec02faf3
Hrvoje Hodak <hhodak@foi.hr>
2019-11-02 14:46:02 +0100

    Ažurirano Idle ponašanje
    Ažurirano Idle ponašanje u vidu animacije od 3 sekunde prije početka kretnje igrača

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -16,17 +16,20 @@ public class PlayerControls : MonoBehaviour
     void Start()
     {
         currentSpeed = 0;
+        acceleration = 0;
         rigidbody2d = GetComponent<Rigidbody2D>();
+        StartCoroutine(Countdown());
     }
 
     // Update is called once per frame
     void Update()
-      {
+    {
         currentSpeed += acceleration;
         if (currentSpeed >= maxSpeed)
         {
             currentSpeed = maxSpeed;
         }
+        animator.SetFloat("Speed", currentSpeed);
 
         if (Input.GetButtonDown("Jump") == true)
         {
@@ -36,10 +39,28 @@ public class PlayerControls : MonoBehaviour
         rigidbody2d.velocity = new Vector2(currentSpeed, rigidbody2d.velocity.y);
     }
 
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+
     private void FixedUpdate()
-      {
+    {
         characterController.Move(rigidbody2d.velocity.x * Time.fixedDeltaTime, false, jump);
         //After a successful jump we need to set jump attribute to the default value = false;
         jump = false;
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("3");
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+        Debug.Log("Go!");
+        acceleration = 0.1f;
     }
 }
