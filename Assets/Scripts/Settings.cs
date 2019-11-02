83d6e173bfc246086a11facaa9941f8353e73121
tjukica <crotrancer@gmail.com>
2019-11-02 14:50:54 +0100

    Napravljen settings menu
    Napravljen je setting menu, koji trenutno samo ima postavke za volume koje nisu povezane sa zvukom, već su vizualni placeholder.

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public GameObject mute, unmute, mainMenu;
+
+    void Start()
+    {
+        volumeSlider.value = volumeSlider.maxValue;
+        mute.SetActive(false);  
+        
+    } 
+
+    public void VolumeChanged(float value)
+    {
+        if(volumeSlider.value > 0)
+        {
+            mute.SetActive(false);
+            unmute.SetActive(true);
+        } else
+        {
+            mute.SetActive(true);
+            unmute.SetActive(false);
+        }
+    }
+    public void Mute(bool value)
+    {
+        if(value)
+        {
+            volumeSlider.value = 0;
+        } else
+        {
+            volumeSlider.value = 1;
+        }
+    }
+}
