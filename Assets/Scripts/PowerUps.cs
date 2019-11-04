88540d74a4f99725730bd134f68ec310b14308dd
ghubak <ghubak@foi.hr>
2019-11-04 16:55:00 +0100

    Dodana mogućnost sakupljanja u inventory
    Ovisno o tagu opreme moguće ju je dodati u invenotry ako u njemu ima mijesta. Trenutno je moguće dodati 1 item al je programska podloga za više njih. Potrebam mi je pristup UI- sučelju za graficki prikaz inventorja

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
+    private Inventory inventory;
+
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -16,6 +24,18 @@ public class PowerUps : MonoBehaviour
 
     private void PickUp()
     {
+        
+        if (gameObject.name== "powerup_collectable")
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.puno[i] == false)
+                {
+                    inventory.puno[i] = true;
+                    Debug.Log("Dodano u inventory");
+                }
+            }
+        }
         Debug.Log("Pokupljeno");
         Destroy(gameObject);
     }
