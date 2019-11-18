45ccff5cffc9876cdb2343ce25a50a559cc506d8
Hrvoje Hodak <hhodak@foi.hr>
2019-11-18 21:29:53 +0100

    Popravljeni bugovi vezani za glavni izbornik
    -Klikom na Start pokreće se igra
    -Klikom na Settings otvaraju se postavke
    -U igri, pritiskom na R, igra ponovno počinje
    -U igri, pritiskom na Esc, korisnik se vraća u glavni izbornik

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Additive);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("WinterRun (Map)");
+        }
+    }
+}
