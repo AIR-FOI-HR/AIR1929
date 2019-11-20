3012aaaa17190d9814912e96de909b6fed14bb54
ghubak <ghubak@foi.hr>
2019-11-20 18:52:29 +0100

    Popravljen raspored elementa po scenama
    Premješteni powerupovi u sample scene.

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -10,7 +10,7 @@ public class PowerUps : MonoBehaviour
 
     private void Start()
     {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Inventory>();
     }
 
     void OnTriggerEnter2D (Collider2D other)
