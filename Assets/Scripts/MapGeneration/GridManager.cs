1ca447fdc75870d801cf417cded8708594fad2ad
tjukica <crotrancer@gmail.com>
2019-12-08 16:28:39 +0100

    Početna faza proceduralne generacije mape
    Napravljene su dvije klase:
    1. GridManager - upravlja stvaranjem mreže koja se popunjava poljima (tile)
    2. Node - predstavlja jedan tile; trenutno samo stvara predefinirani GO (prefab) koji se koristi za popunjavanje mreže

---
+++ Assets/Scripts/MapGeneration/GridManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int rows = 20;
+    public int cols = 100;
+    public float tileSize = 1;
+
+    void Start()
+    {        
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        GameObject referenceTile = (GameObject)Resources.Load("TileDirt");
+        Node node = referenceTile.AddComponent<Node>();
+        for (int col = 0; col < cols; col++)
+        {            
+            for (int row = 0; row < rows; row++)
+            {
+               
+                referenceTile = node.CreateNode(Node.TileType.snow);
+                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+                tile.name ="("+ col + ", " + row + ")";
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/MapGeneration/Node.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    void Start()
+    {
+ 
+    }
+
+    public GameObject CreateNode(TileType type)
+    {
+        GameObject referenceTile = null;
+        switch (type)
+        {
+            case TileType.dirt:
+                referenceTile = (GameObject)Instantiate(Resources.Load("TileDirt"));
+                break;
+            case TileType.snow:
+                referenceTile = (GameObject)Instantiate(Resources.Load("TileSnow"));
+                break;
+
+        }    
+        return referenceTile;
+    }
+
+    public TileType tileType;
+    public enum TileType
+    {
+        dirt,
+        snow
+    }
+
+}
