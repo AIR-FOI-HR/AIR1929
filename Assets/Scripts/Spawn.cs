a7808f187d0a112ec1a9028f90f90c8428f1afb4
ghubak <ghubak@foi.hr>
2019-12-10 23:33:28 +0100

    Skripte Spawn i Slots
    Za korištenje pojaćanja.Trenutno samo nestane iz inventoria i pojavi se na mapi al se ne prikazuje. #19

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -14,7 +14,7 @@ public class Spawn : MonoBehaviour
 
     public void SpawnItem()
     {
-        Vector2 playerPos = new Vector2(player.position.x, player.position.y-3);
+        Vector2 playerPos = new Vector2(player.position.x-3, player.position.y);
         Instantiate(item,playerPos,Quaternion.identity);
     }
 }
