06497b02d7c68ee24b1c639aca8183873e689498
ghubak <ghubak@foi.hr>
2019-12-16 14:54:52 +0100

    Dodano da se pojavljuju u pravoj rezoliciji
    Transormiranje elementa u kodu na zadanu veličinu. #19

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -14,7 +14,11 @@ public class Spawn : MonoBehaviour
 
     public void SpawnItem()
     {
+        Vector3 vektor = new Vector3(1, 1, 1);
         Vector2 playerPos = new Vector2(player.position.x-3, player.position.y);
+        item.transform.localScale = vektor;
         Instantiate(item,playerPos,Quaternion.identity);
+        
+
     }
 }
