9775310aa4ffb87f5427d25f153e2c34ecfa1e98
Domagoj Hamzić <dhamzic797@gmail.com>
2019-12-17 22:18:50 +0100

    Spremanje i čitanje lokalnih podataka
    Spremanje vremena prolaska staze te prikazivanje istih u LeaderBoard-u nakon utrke.

---
+++ Assets/Scripts/ERA/Score.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.ERA
+{
+    /// <summary>
+    /// Lokalno spremanje najboljih rezultata
+    /// </summary>
+    [Serializable]
+    public class Score
+    {
+        /// <summary>
+        /// Vrijeme prolaska staze
+        /// </summary>
+        public float RaceTime;
+        /// <summary>
+        /// Datum i vrijeme utrke
+        /// </summary>
+        public DateTime RunDate;
+        /// <summary>
+        /// Mapa utrke
+        /// </summary>
+        public Map Map;
+        /// <summary>
+        /// Postavke i igrač koji se korišteni za navedenu stazu.
+        /// </summary>
+        public PlayerSettings PlayerSettings;
+    }
+}
