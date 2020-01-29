e2279ea3f00de80dbefa15fb39564d22ecb6463e
Ivan Katoliković <ikatoliko@foi.hr>
2020-01-29 21:13:51 +0100

    Dodana znacka za skakanje 15 puta u mapi WinterRun
    Dodana znacka za skakanje 15 puta u mapi winter run (jos nije implementiran do kraja, uoceni nedostatci kod trenutne implementacije)

---
+++ Assets/Scripts/Achievements/Achievements/WinterRunJumper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinterRunJumper : MonoBehaviour, IAchievement {
+	public string Name { get; set; } = "The floor is lava! But cold...";
+	public string Description { get; set; } = "Jump 15 times while playing on \"Winter Run\" map.";
+	public Sprite ImageAchieved { get; set; }
+	public Sprite ImageNotAchieved { get; set; }
+	public bool Achieved { get; set; }
+	public string TargetObjectName { get; set; } = "FemaleNinja";
+
+	public void Initialize() {
+		if (PPAchivements.LoadAchievement(Name) < 15) {
+			Achieved = false;
+		}
+		else Achieved = true;
+	}
+
+	
+}
